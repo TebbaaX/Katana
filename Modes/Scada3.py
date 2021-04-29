5282a745872d1a1f8841931861c7c61ad2be857b
Mohaned Abd Elmonsef <Mo0han3d@gmail.com>
2021-04-30 00:23:11 +0200

    auto removing google cookie in scade mode

--- Modes/Scada3.py
+++ Modes/Scada3.py
@@ -36,23 +36,30 @@ from googlesearch import search
 import sys
 from termcolor import colored, cprint
 import random
-from http import cookiejar  
+from http import cookiejar
+
+
 class BlockAll(cookiejar.CookiePolicy):
-    return_ok = set_ok = domain_return_ok = path_return_ok = lambda self, *args, **kwargs: False
+    return_ok = set_ok = domain_return_ok = path_return_ok = lambda self, * \
+        args, **kwargs: False
     netscape = True
     rfc2965 = hide_cookie2 = False
-    
-TLD = ["com.qa","ru","com.sa"]
+
+
+TLD = ["com.qa", "ru", "com.sa"]
 
 lux = random.choice(TLD)
 
 Vx = """ inurl:/Portal0000.htm """
 query = Vx
 # ****
+
+
 def spinning_cursor():
     while True:
         for cursor in '|/-\\':
             yield cursor
+
 
 spinner = spinning_cursor()
 for _ in range(100):
@@ -60,16 +67,10 @@ for _ in range(100):
     sys.stdout.flush()
     time.sleep(0.1)
     sys.stdout.write('\b')
-#*****
-for gamma in search(query, tld=lux, num=10,stop=50,pause=2): 
-     print(colored ('[+] Found > ' ,'yellow')  + (gamma) )   
-print(colored('[+] 90% done ', 'green' ))
-print(colored('[+] 100% done ', 'green' ))
-print(colored('[+] done ', 'green' ))
-print(colored ('[! >] delete .google-cookie file in Katana DIR  ' ,'red')) 
-
-
-
-
-
-
+# *****
+for gamma in search(query, tld=lux, num=10, stop=50, pause=2):
+    print(colored('[+] Found > ', 'yellow') + (gamma))
+print(colored('[+] 90% done ', 'green'))
+print(colored('[+] 100% done ', 'green'))
+print(colored('[+] done ', 'green'))
+os.remove("./.google-cookie")
